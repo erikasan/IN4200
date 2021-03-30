f10cef5dddde12dab0f2ea4efb27d3dc3781b4f7
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-03-30 23:35:51 +0200

    Update

--- home_exam_1/header_files/check_node.h
+++ home_exam_1/header_files/check_node.h
@@ -5,7 +5,7 @@ using namespace std;
 void check_node(int node_id, int tau, int N, int *row_ptr, int *col_idx, int *SNN_val){
   vector<int> queue;
   int discovered[N]{};
-
+  
   bool no_cluster = true;
   int node, discovered_node;
 
