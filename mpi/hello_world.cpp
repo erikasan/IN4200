1f37f14cf497460e5526f84f76d4f78e06c1f5b9
erikasan <erikasan@student.matnat.uio.no>
2021-04-14 13:13:34 +0200

    simple hello world

---
+++ mpi/hello_world.cpp
@@ -0,0 +1,17 @@
+#include <mpi.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv){
+
+  MPI_Init(&argc, &argv);
+
+  int rank;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  cout << "Hello world from process " << rank << "!" << endl;
+  MPI_Finalize();
+
+  return 0;
+}
