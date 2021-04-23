b69614ad3fa697e5e266c74f0e13025c4cfd0736
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-04-23 15:27:00 +0200

    update

--- exercise_set_10/test.cpp
+++ exercise_set_10/test.cpp
@@ -1,12 +1,26 @@
+#include <mpi.h>
 #include <iostream>
 #include <cstdio>
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-  char message[20];
-  sprintf(message, "This is a test");
-  cout << message << endl;
+  int rank, size;
+  int root = 0;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  int B[3];
+  if (rank == 0){
+    int A[] = {1,2,3};
+  }
+
+  MPI_Scatter(A, 1, MPI_INT, B, 1, root, MPI_COMM_WORLD);
+
+  cout << B[0] << endl;
+
+  MPI_Finalize();
   return 0;
 }
