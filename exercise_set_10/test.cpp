4857299949e25eac46a3a0b17d613d1af2d13157
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-04-23 15:27:57 +0200

    update

--- exercise_set_10/test.cpp
+++ exercise_set_10/test.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  int B[3];
+  int A[3], B[3];
   if (rank == 0){
     int A[] = {1,2,3};
   }
