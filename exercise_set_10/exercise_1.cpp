75212eaa7223afbf40001a9168bd5ea0ed6a1789
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-04-23 12:03:25 +0200

    test

--- exercise_set_10/exercise_1.cpp
+++ exercise_set_10/exercise_1.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  int message_len = 40;
+  int message_len = 26;
   char message[message_len];
   MPI_Status *status;
 
