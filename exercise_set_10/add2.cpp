3385fd05c48b2dc131aff31a59b68b0cd7022020
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-04-23 14:10:51 +0200

    update

--- exercise_set_10/add2.cpp
+++ exercise_set_10/add2.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_size, &size);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   double A[size]; // pregather
   double B[size]; // postgather
