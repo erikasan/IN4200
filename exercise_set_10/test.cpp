6a7cddac1e08d34dd8723eba4ffa77933385a4f7
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-04-23 11:54:05 +0200

    Test

---
+++ exercise_set_10/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+  char message[20];
+  sprintf(message, "This is a test");
+  cout << message << endl;
+  return 0;
+}
