8523afa1585eca6ef98e081ed8727266cc174dda
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-04-24 11:23:57 +0200

    update

---
+++ exercise_set_10/matrix_vector_mult.cpp
@@ -0,0 +1,47 @@
+#include <mpi.h>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void mat_vec_mult(int *A, int *x, int *y, int N);
+
+int main(int argc, char **argv)
+{
+  int i, j;
+  int rank, size;
+  int root = 0;
+
+  int N = atoi(argv[1]);
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  if (rank == root){
+    int A[N][N]{}, x[N];
+
+    for (i = 0; i < N; i++){
+      A[i][i] = 1;
+      x[i] = i;
+    }
+  }
+
+  // Broadcast x to all processes
+  MPI_Bcast(x, N, MPI_INT, root, MPI_COMM_WORLD);
+
+  // Split up A 
+  //MPI_Scatter();
+
+  MPI_Finalize();
+  return 0;
+}
+
+void mat_vec_mult(int *A, int *x, int *y, int N)
+{
+  *y = 0;
+  for (int j = 0; j < N; j++){
+    *y += A[j]*x[j];
+  }
+  return;
+}
