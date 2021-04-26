0cf517a05c8e0be7fb7ff75f05bdc1199c90b704
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-04-26 13:58:52 +0200

    update

--- exercise_set_10/matrix_vector_mult.cpp
+++ exercise_set_10/matrix_vector_mult.cpp
@@ -47,13 +47,16 @@ int main(int argc, char **argv)
   int Sdispls[N];
   int Gdispls[N];
 
+  Sdispls[0] = 0;
+  Gdispls[0] = 0;
+
   for (i = 0; i < size-1; i++){
-    n_rows[i] = rows; //+ ((i >= (size - remainder)) ? 1:0);
+    n_rows[i] = rows + ((i >= (size - remainder)) ? 1:0);
     sendcounts[i] = n_rows[i]*N;
     Sdispls[i+1] = Sdispls[i] + sendcounts[i];
     Gdispls[i+1] = Gdispls[i] + n_rows[i];
   }
-  n_rows[size-1] = rows + remainder;//+ ((size-1) >= (size - remainder) ? 1:0);
+  n_rows[size-1] = rows + ((size-1) >= (size - remainder) ? 1:0);
   sendcounts[size-1] = n_rows[size-1]*N;
 
   MPI_Scatterv(A, sendcounts, Sdispls, MPI_DOUBLE, A, N*n_rows[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
