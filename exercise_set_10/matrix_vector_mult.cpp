92b69846c9a1606cdbb612ff4eb5693b2c40c145
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-04-26 11:17:46 +0200

    update

--- exercise_set_10/matrix_vector_mult.cpp
+++ exercise_set_10/matrix_vector_mult.cpp
@@ -12,7 +12,8 @@ int main(int argc, char **argv)
   int rank, size;
   int root = 0;
 
-  int N = atoi(argv[1]);
+  cout << "Enter N = " << endl;
+  cin >> N;
 
   double **A, *x;
 
@@ -20,29 +21,29 @@ int main(int argc, char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  // if (rank == root){
-  //   A = new double*[N];
-  //   x = new double[N];
-  //
-  //   for (i = 0; i < N; i++){
-  //     A[i] = new double[N]{};
-  //     A[i][i] = 1;
-  //     x[i] = i;
-  //   }
-  // }
+  if (rank == root){
+    A = new double*[N];
+    x = new double[N];
 
-  // Broadcast x to all processes
-  //MPI_Bcast(x, N, MPI_INT, root, MPI_COMM_WORLD);
+    for (i = 0; i < N; i++){
+      A[i] = new double[N]{};
+      A[i][i] = 1;
+      x[i] = i;
+    }
+  }
 
-  // Test that broadcast works
-  // for (i = 0; i < size; i++){
-  //   if (rank == i){
-  //     for (j = 0; j < N; j++){
-  //       cout << x[j] << " ";
-  //     }
-  //     cout << endl;
-  //   }
-  // }
+  Broadcast x to all processes
+  MPI_Bcast(x, N, MPI_INT, root, MPI_COMM_WORLD);
+
+  Test that broadcast works
+  for (i = 0; i < size; i++){
+    if (rank == i){
+      for (j = 0; j < N; j++){
+        cout << x[j] << " ";
+      }
+      cout << endl;
+    }
+  }
 
   // Split up A
   //MPI_Scatter();
