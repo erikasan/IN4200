37030b6b72fb523e02374b9ebe757c3bdf92c9e3
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-05-01 12:53:06 +0200

    update

--- home_exam_2/MPI_main.cpp
+++ home_exam_2/MPI_main.cpp
@@ -14,31 +14,62 @@ int main(int nargs, char **args)
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   if (rank == 0){
+    size_t i, j;
+
     // read from command line the values of M, N, and K
     M = atoi(argv[1]);
     N = atoi(argv[2]);
     K = atoi(argv[3]);
 
     // allocate 2D array 'input' with M rows and N columns
-
+    input = new float*[M];
+    for (i = 0; i < M; i++){
+      input[i] = new float[N];
+    }
 
     // allocate 2D array 'output' with M - K + 1 rows and N - K + 1 columns
+    output = new float*[M - K + 1];
+    for (i = 0; i < M - K + 1; i++){
+      output[i] = new float[M - K + 1];
+    }
+
     // allocate the convolutional kernel with K rows and K columns
+    kernel = new float*[K];
+    for (i = 0; i < K; i++){
+      kernel[i] = new float[K];
+    }
+
     // fill 2D array 'input' with some values
+    for (i = 0; i < M; i++){
+    for (j = 0; j < N; j++){
+      input[i][j] = 1;
+    }}
+
     // fill kernel with some values
-    // ....
+    for (i = 0; i < K; i++){
+    for (j = 0; j < K; j++){
+      kernel[i][j] = 1;
+    }}
   }
 
   // process 0 broadcasts values of M, N, K to all other processes
-  // ....
+  MPI_Bcast(&M, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&K, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
   if (rank > 0){
+    size_t i;
+
     // allocate the convolutional kernel with K rows and K columns
-    // ...
+    kernel = new float*[K];
+    for (i = 0; i < K; i++){
+      kernel[i] = new float[K];
+    }
   }
 
   // process 0 broadcasts the content of the kernel to all other processes
   // ...
+  //MPI_Bcast(&kernel, K*K, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
   // parallel computation of a single-layer convolution
   // MPI_single_layer_convolution(M, N, input, K, kernel, output);
---
+++ home_exam_2/test.cpp
@@ -0,0 +1,44 @@
+#include <mpi.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int nargs, char **args)
+{
+  size_t i, j;
+  int M = 2, N = 2, rank;
+  double **A = NULL;
+
+  MPI_Init(&nargs, &args);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  if (rank == 0){
+    A = new double*[M];
+    for (i = 0; i < M; i++){
+      A[i] = new double[N]{};
+      A[i][i] = 1;
+    }
+  }
+
+  if (rank > 0){
+    A = new double*[M];
+    for (i = 0; i < M; i++){
+      A[i] = new double[N];
+    }
+  }
+
+  MPI_Bcast(&A, M*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+  if (rank > 0){
+    cout << "A on process " << rank << endl;
+    for (i = 0; i < M; i++){
+      for (j = 0; j < N; j++){
+        cout << A[i][j] << " ";
+      }
+      cout << endl << endl;
+    }
+  }
+
+  MPI_Finalize();
+  return 0;
+}
