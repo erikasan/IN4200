8a80d9e19ae84516d4aef2912e4824ab98814da4
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-05-01 13:05:59 +0200

    update

--- home_exam_2/test.cpp
+++ home_exam_2/test.cpp
@@ -6,11 +6,12 @@ using namespace std;
 int main(int nargs, char **args)
 {
   size_t i, j;
-  int M = 2, N = 2, rank;
+  int M = 2, N = 2, rank, size;
   double **A = NULL;
 
   MPI_Init(&nargs, &args);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   if (rank == 0){
     cout << "A allocated on process " << rank << endl;
@@ -29,15 +30,20 @@ int main(int nargs, char **args)
     }
   }
 
-  MPI_Bcast(&A, M*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Bcast(A, M*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-  cout << "A on process " << rank << endl;
-  for (i = 0; i < M; i++){
-    for (j = 0; j < N; j++){
-      cout << A[i][j] << " ";
+  for (i = 0; i < size; i++){
+    if (rank == i){
+      cout << "A on process " << rank << endl;
+      for (i = 0; i < M; i++){
+        for (j = 0; j < N; j++){
+          cout << A[i][j] << " ";
+        }
+        cout << endl << endl;
+      }
     }
-    cout << endl << endl;
   }
+
 
   MPI_Finalize();
   return 0;
