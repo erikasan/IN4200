3d1a6e5a8ae5a58e7a6dedcc1636a0c187321101
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-05-01 15:22:01 +0200

    update

--- home_exam_2/MPI_single_layer_convolution.cpp
+++ home_exam_2/MPI_single_layer_convolution.cpp
@@ -3,5 +3,11 @@ void MPI_single_layer_convolution(int M, int N, float **input,
                                   int K, float **kernel,
                                   float **output)
 {
+
+  // Calculate how many rows of input each process receives
+
+  // MPI_Scatterv
+
+  // MPI_Gatherv
   return;
 }
