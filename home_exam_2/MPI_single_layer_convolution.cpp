6440853189c75c29a2a095c299bc972b17b18b24
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-05-03 12:06:16 +0200

    test

--- home_exam_2/MPI_single_layer_convolution.cpp
+++ home_exam_2/MPI_single_layer_convolution.cpp
@@ -100,9 +100,9 @@ void MPI_single_layer_convolution(int M, int N, float **input,
   }}
 
   // MPI_Gatherv
-  // MPI_Gatherv(output[0], Gcounts[rank], MPI_FLOAT,
-  //             output[0], Gcounts, Gdispls, MPI_FLOAT,
-  //             0, MPI_COMM_WORLD);
+  MPI_Gatherv(output[0], Gcounts[rank], MPI_FLOAT,
+              output[0], Gcounts, Gdispls, MPI_FLOAT,
+              0, MPI_COMM_WORLD);
 
   return;
 }
