1c705f47e168dca6c3143ec81b27f2e050979169
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-05-03 10:16:04 +0200

    test

--- home_exam_2/MPI_main.cpp
+++ home_exam_2/MPI_main.cpp
@@ -84,7 +84,7 @@ int main(int nargs, char **args)
     // produced by the sequential function single_layer_convolution
     // ...
 
-    for (i = 0; i < M - K + 1); i++){
+    for (i = 0; i < M - K + 1; i++){
       for (j = 0; j < N - K + 1; j++){
         cout << output[i][j] << " ";
       }
