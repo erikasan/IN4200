2edcf97cf073da7feb7181d4d9aab1ed4c676541
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-05-04 13:12:35 +0200

    test

--- home_exam_2/MPI_double_layer_convolution.cpp
+++ home_exam_2/MPI_double_layer_convolution.cpp
@@ -1,5 +1,5 @@
 
-void MPI_double_layer_convolution(int M, int N, float **input
+void MPI_double_layer_convolution(int M, int N, float **input,
                                   int K1, float **kernel1,
                                   int K2, float **kernel2,
                                   int **output)
