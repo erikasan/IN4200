17a66d9957bc5964f13f82719aa4ebfe68f6021f
Erik Alexander Sandvik <e.a.sandvik@fys.uio.no>
2022-04-19 14:57:27 +0200

    test

--- mpi/test.c
+++ mpi/test.c
@@ -8,23 +8,25 @@ int main(int argc, char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  int A[4];
+  int A[4], B[4];
 
-  // Initialize array
   for (i = 0; i < 4; i++){
     A[i] = 0;
+    B[i] = 0;
   }
 
-  if (rank == 0){
+  int root = 0;
+
+  if (rank == root){
     A[0] = 3;
     A[1] = 5;
     A[2] = 4;
     A[3] = 1;
   }
 
-  MPI_Bcast(A, 4, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Scatter(A, 1, MPI_INT, B, 1, MPI_INT, root, MPI_COMM_WORLD);
 
-  printf("Rank %d: A = {%d, %d, %d, %d} \n", rank, A[0], A[1], A[2], A[3]);
+  printf("Rank %d: B = {%d, %d, %d, %d} \n", rank, B[0], B[1], B[2], B[3]);
 
   MPI_Finalize();
   return 0;
