c37df8e9dfe4d81e639c92ab529c39cc49502ed7
Erik Alexander Sandvik <e.a.sandvik@fys.uio.no>
2022-04-30 14:11:21 +0200

    test

--- oblig_2/parallel_code/functions.c
+++ oblig_2/parallel_code/functions.c
@@ -53,7 +53,7 @@ void convert_image_to_jpeg(const image *u, unsigned char* image_chars){
 
     for (i = 0; i < m; i++){
         for (j = 0; j < n; j++){
-            image_chars[i*n + j] = (*u).image_data[i][j];
+            //image_chars[i*n + j] = (*u).image_data[i][j];
             //printf("%d %d %f \n", i, j, (*u).image_data[i][j]);
         }
     }
