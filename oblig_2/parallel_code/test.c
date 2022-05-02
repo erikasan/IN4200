ea496fdb8244606a7a13ee1f9918c40d72747616
Erik Alexander Sandvik <e.a.sandvik@fys.uio.no>
2022-05-02 17:47:09 +0200

    test

---
+++ oblig_2/parallel_code/test.c
@@ -0,0 +1,58 @@
+#include <mpi.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int rank, size;
+
+    int m = 3, n = 3;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0){
+
+        float **A;
+
+        A = (float **) malloc(m*sizeof(float *));
+        for (int i = 0; i < m; i++){
+            A[i] = (float *) malloc(n*sizeof(float));
+        }
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                A[i][j] = i + j;
+            }
+        }
+
+        MPI_Send(A, m*n, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
+    }
+
+    if (rank == 1){
+
+        float **B; 
+
+        B = (float **) malloc(m*sizeof(float *));
+        for (int i = 0; i < m; i++){
+            B[i] = (float *) malloc(n*sizeof(float));
+        }
+
+        MPI_Status status;
+        MPI_Recv(B, m*n, MPI_FLOAT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                printf("%f ", B[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    
+
+
+    MPI_Finalize();
+    return 0;
+}
