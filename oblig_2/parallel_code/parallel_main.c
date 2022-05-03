8ba4e63ef8b1e8e908c449a857bea9f69d16b74e
Erik Alexander Sandvik <e.a.sandvik@fys.uio.no>
2022-05-03 11:42:53 +0200

    test

--- oblig_2/parallel_code/parallel_main.c
+++ oblig_2/parallel_code/parallel_main.c
@@ -93,7 +93,7 @@ int main(int argc, char *argv[])
   
 
   convert_jpeg_to_image(my_image_chars, &u);
-  //iso_diffusion_denoising_parallel(&u, &u_bar, kappa, iters);
+  iso_diffusion_denoising_parallel(&u, &u_bar, kappa, iters);
 
   // // Each process sends its resulting content of u_bar to process 0
   // // Process 0 receives from each process incoming values and
