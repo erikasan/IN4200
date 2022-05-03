686105dab0700df56dc1266a9e06b6d06cfcefcc
Erik Alexander Sandvik <e.a.sandvik@fys.uio.no>
2022-05-03 14:31:36 +0200

    test

--- oblig_2/parallel_code/test.c
+++ oblig_2/parallel_code/test.c
@@ -2,66 +2,101 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void allocate_array2d(double **x, int m, int n);
+
+void copy_u_to_u_bar(double **u, double **u_bar, int m, int n);
+
+void print_array2d(double **x, int m, int n);
+
 int main(int argc, char *argv[])
 {
     int rank, size;
 
-    int m = 3, n = 3;
+    int m = 2, n = 2;
 
     MPI_Init(&argc, &argv);
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    double **u, **u_bar;
+    allocate_array2d(u, m, n);
+    allocate_array2d(u_bar, m, n);
+
     if (rank == 0){
+        double **whole_array;
+        allocate_array2d(whole_array, 2*m, 2*n);
 
-        float **A;
-
-        A = malloc(m * sizeof *A);
-        A[0] = malloc(m*n * sizeof *A[0]); 
-        for (int i = 1; i < m; i++){
-            A[i] = &(A[0][i*n]);
-        }
-
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                A[i][j] = i + j;
+        for (int i = 0; i < 2*m; i++){
+            for (int j = 0; j < 2*n; j++){
+                whole_array[i][j] = i + j;
             }
         }
 
-        MPI_Send(A[0], m*n, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
+        int counts_send[num_procs];
+        counts_send[0] = m;
+        counts_send[1] = m;
 
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                printf("%f ", A[i][j]);
-            }
-            printf("\n");
-        }
+        int displacements[num_procs];
+        displacements[0] = 0;
+        displacements[1] = m;
     }
 
-    if (rank == 1){
+    MPI_Scatterv(whole_array[0],
+                 counts_send,
+                 displacements,
+                 MPI_DOUBLE,
+                 u,
+                 m*n,
+                 MPI_DOUBLE,
+                 0,
+                 MPI_COMM_WORLD);
 
-        float **B; 
+    copy_u_to_u_bar(u, u_bar, m, n);
 
-        B = malloc(m * sizeof *B);
-        B[0] = malloc(m*n * sizeof *B[0]); 
-        for (int i = 1; i < m; i++){
-            B[i] = &(B[0][i*n]);
-        }
+    MPI_Gatherv(u_bar[0],
+                m*n,
+                MPI_DOUBLE,
+                whole_array[0],
+                counts_send,
+                displacements,
+                MPI_DOUBLE,
+                0,
+                MPI_COMM_WORLD);
 
-        MPI_Status status;
-        MPI_Recv(B[0], m*n, MPI_FLOAT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                printf("%f ", B[i][j]);
-            }
-            printf("\n");
-        }
+    if (rank == 0){
+        print_array2d(whole_array, 2*m, 2*n);
     }
-    
-
 
     MPI_Finalize();
     return 0;
+}
+
+void allocate_array2d(double **x, int m, int n){
+    x = malloc(m * sizeof *x);
+    x[0] = malloc(m*n * sizeof *x[0]);
+    for (int i = 1; i < m; i++){
+        x[i] = &(x[0][i*n]);
+    }
+    return;
+}
+
+
+void copy_u_to_u_bar(double **u, double **u_bar, int m, int n){
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            u_bar[i][j] = u[i][j];
+        }
+    }
+    return;
+}
+
+void print_array2d(double **x, int m, int n){
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            printf("%f ", x[i][j]);
+        }
+        printf("\n");
+    }
+    return;
 }
